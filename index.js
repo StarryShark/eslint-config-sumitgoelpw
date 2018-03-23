84b754154df7a6624814ee930e9c530c1163dabd
Sumit Goel <sumitgoelpw@users.noreply.github.com>
2018-03-22 18:46:49 -0700

    move rules from dot file to index.js

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
-/**
- * Dependencies
- */
-module.exports = require('./.eslintrc.js');
+module.exports = {
+  'env': {
+    'node': true,
+    'es6': true,
+    'mocha': true,
+    'jasmine': true
+  },
+  'extends': 'eslint:all',
+  'parserOptions': {
+    'ecmaVersion': 9,
+    'sourceType': 'module'
+  },
+  'plugins': ['promise'],
+  'rules': {
+    'indent': [
+      'error',
+      2,
+      {'SwitchCase': 1}
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'dot-location': [
+      'error',
+      'property'
+    ],
+    'one-var': [
+      'error',
+      'never'
+    ],
+    'no-magic-numbers': 'off',
+    'no-console': 'off',
+    'sort-keys': 'off',
+    'no-inline-comments': 'off',
+    'no-process-env': 'off',
+    'max-params': [
+      'error',
+      {'max': 4}
+    ],
+    'padded-blocks': [
+      'error',
+      {
+        'blocks': 'never',
+        'switches': 'never'
+      }
+    ],
+    'max-statements': [
+      'error',
+      11,
+      {'ignoreTopLevelFunctions': true}
+    ]
+  }
+};
